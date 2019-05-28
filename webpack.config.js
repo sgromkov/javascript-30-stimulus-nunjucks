198531399ead3d0675e54b3018ca49c3be9622c3
Sergey Gromkov <sgromkov@gmail.com>
2019-05-28 16:55:17 +0300

    add multiple entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const glob = require('glob');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
@@ -11,25 +12,47 @@ function module_exists(name) {
     catch (e) { return false }
 }
 
-module.exports = env => {
-    const data = (module_exists(`./src/${env.volume}/data.js`)) ? require(`./src/${env.volume}/data.js`) : {};
+var entryObject = glob.sync('./src/**/index.js').reduce(
+    function (entries, entry) {
+        var matchForRename = /^\.\/src\/([\w\d_]+\/index)\.js$/g.exec(entry);
+        var volume = `${matchForRename[1]}/`;
 
+        if (matchForRename !== null && typeof matchForRename[1] !== 'undefined') {
+            entries[matchForRename[1]] = entry;
+        }
+
+        return entries;
+    },
+    {}
+);
+
+var templateArray = glob.sync('./src/**/index.njk').reduce(
+    function (templates, template) {
+        var matchForRename = /^\.\/src\/([\w\d_])+\/index\.njk$/g.exec(template);
+
+        if (matchForRename !== null && typeof matchForRename[1] !== 'undefined') {
+            templates.push({
+                from: `./src/${matchForRename[1]}/index.njk`,
+                to: path.resolve(__dirname, `dist/${matchForRename[1]}/index.html`),
+                context: (module_exists(`./src/${matchForRename[1]}/data.js`)) ? require(`./src/${matchForRename[1]}/data.js`) : {},
+                writeToFileEmit: true
+            });
+        }
+
+        return templates;
+    },
+    []
+);
+
+module.exports = env => {
     return {
-        entry: `./src/${env.volume}/index.js`,
+        entry: entryObject,
         output: {
-            filename: 'main.js',
-            path: path.resolve(__dirname, `dist/${env.volume}`)
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
         },
         mode: 'development',
         devtool: 'inline-source-map',
-        devServer: {
-            // contentBase: path.join(__dirname, `dist/${env.volume}`),
-            contentBase: path.join(__dirname, 'dist'),
-            // publicPath: '/1/',
-            hot: true,
-            port: 3000,
-            open: true
-        },
         optimization: {
             minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
         },
@@ -44,7 +67,9 @@ module.exports = env => {
                     test: /\.css$/,
                     use: [
                         MiniCssExtractPlugin.loader,
-                        'css-loader'
+                        {
+                            loader: 'css-loader',
+                        }
                     ]
                 },
                 {
@@ -53,7 +78,9 @@ module.exports = env => {
                         {
                             loader: 'file-loader',
                             options: {
-                                name: '[name].[ext]'
+                                name: '[path][name].[ext]',
+                                context: 'src',
+                                publicPath: '../'
                             }
                         }
                     ]
@@ -65,17 +92,10 @@ module.exports = env => {
                 filename: '[name].css'
             }),
             new NunjucksWebpackPlugin({
-                templates: [
-                    {
-                        from: `./src/${env.volume}/index.njk`,
-                        to: path.resolve(__dirname, `dist/${env.volume}/index.html`),
-                        context: data,
-                        writeToFileEmit: true
-                    }
-                ]
+                templates: templateArray
             }),
             new CleanWebpackPlugin(),
-            new webpack.HotModuleReplacementPlugin()
+            // new webpack.HotModuleReplacementPlugin()
         ]
     };
 };
